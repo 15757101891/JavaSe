d884ada1e722d3fab49740c0c3bb7ddd672ee350
xjm <825635918@qq.com>
2015-10-21 17:16:19 +0800

    01

---
+++ src/cn/zucc/day01/ShoppingSystem.java
@@ -0,0 +1,18 @@
+package cn.zucc.day01;
+
+public class ShoppingSystem {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\n");
+		System.out.println("\t\t\t欢迎使用我行我素购物管理系统\n\n");
+		System.out.println("\t\t\t\t1.登陆系统\n\n");
+		System.out.println("\t\t\t\t2.退出\n\n");
+		System.out.println("**************************************************************************************************\n");
+		System.out.println("请选择，输入数字：");
+	}
+
+}
